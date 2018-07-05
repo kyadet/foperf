2dedb58e141ec62708895c29734844b9994fb5eb
kyadet <ryuichisaeki@ruor.com>
2018-07-05 12:04:26 -0400

    add execute scripts

---
+++ wake_relay.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nohup ./bin/relay --sport :7000 -dport :7001 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7002 -dport :7003 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7004 -dport :7005 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7006 -dport :7007 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7008 -dport :7009 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7010 -dport :7011 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7012 -dport :7013 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7014 -dport :7015 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7016 -dport :7017 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7018 -dport :7019 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7020 -dport :7021 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7022 -dport :7023 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7024 -dport :7025 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7026 -dport :7027 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7028 -dport :7029 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7030 -dport :7031 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7032 -dport :7033 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7034 -dport :7035 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7036 -dport :7037 1> /dev/null 2>&1 &
+nohup ./bin/relay --sport :7038 -dport :7039 1> /dev/null 2>&1 &

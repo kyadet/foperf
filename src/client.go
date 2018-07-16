e9079401ed8a7d46e37ba0f8cf05f9a0975c0779
kyadet <ryuichisaeki@ruor.com>
2018-07-16 10:49:24 -0400

    fmt, clean imports

--- src/client.go
+++ src/client.go
@@ -3,56 +3,56 @@ package main
 import (
 	//"bufio"
 	//"os"
+	"flag"
+	"fmt"
+	"github.com/zeromq/goczmq"
+	"log"
 	"strconv"
 	"strings"
-	"log"
 	"time"
-	"flag"
-	"github.com/zeromq/goczmq"
-	"fmt"
 )
 
 type client struct {
-    Filter string
-    Id int
-    Seq int
-    Seqgap []int
-    Msg string
-    Deal *goczmq.Sock
-    Sub *goczmq.Sock 
-    Err error
+	Filter string
+	Id     int
+	Seq    int
+	Seqgap []int
+	Msg    string
+	Deal   *goczmq.Sock
+	Sub    *goczmq.Sock
+	Err    error
 }
 
 var msgSize = 740
 var interval = 500
 
 var (
-    wakeIntval int
-    recvIntval int
-    destAddr string
-    destDealSchema string
-    destDealPort string
-    destSubSchema string
-    destSubPort string
-    wakeCount int
-    logLevel int
-    fanoutRatio int
-    subFilter int
+	wakeIntval     int
+	recvIntval     int
+	destAddr       string
+	destDealSchema string
+	destDealPort   string
+	destSubSchema  string
+	destSubPort    string
+	wakeCount      int
+	logLevel       int
+	fanoutRatio    int
+	subFilter      int
 )
 
 func param() {
-    flag.IntVar(&wakeIntval, "wakeint", 1000, "wake client interval (milliseconds)")
-    flag.IntVar(&recvIntval, "recvint", 500, "receive message interval (milliseconds)")
-    flag.StringVar(&destAddr, "addr","127.0.0.1", "destination address")
-    flag.StringVar(&destDealSchema, "dschm","tcp://", "destination dealer schema tcp or udp")
-    flag.StringVar(&destDealPort, "dport",":7001", "destination dealer port")
-    flag.StringVar(&destSubSchema, "sschm","tcp://", "destination subscribe schema tcp or udp")
-    flag.StringVar(&destSubPort, "sport",":7000", "destination subscribe port")
-    flag.IntVar(&wakeCount, "wake",5, "wake client")
-    flag.IntVar(&logLevel, "log",0 ,"loglevel ... 0=lostonly, 1=verbose")
-    flag.IntVar(&fanoutRatio, "ratio",0 ,"set fanout ratio, 0 is generic test start")
-    flag.IntVar(&subFilter, "subfilter",0 ,"subscribe filter 0 is nofilter")
-    flag.Parse()
+	flag.IntVar(&wakeIntval, "wakeint", 1000, "wake client interval (milliseconds)")
+	flag.IntVar(&recvIntval, "recvint", 500, "receive message interval (milliseconds)")
+	flag.StringVar(&destAddr, "addr", "127.0.0.1", "destination address")
+	flag.StringVar(&destDealSchema, "dschm", "tcp://", "destination dealer schema tcp or udp")
+	flag.StringVar(&destDealPort, "dport", ":7001", "destination dealer port")
+	flag.StringVar(&destSubSchema, "sschm", "tcp://", "destination subscribe schema tcp or udp")
+	flag.StringVar(&destSubPort, "sport", ":7000", "destination subscribe port")
+	flag.IntVar(&wakeCount, "wake", 5, "wake client")
+	flag.IntVar(&logLevel, "log", 0, "loglevel ... 0=lostonly, 1=verbose")
+	flag.IntVar(&fanoutRatio, "ratio", 0, "set fanout ratio, 0 is generic test start")
+	flag.IntVar(&subFilter, "subfilter", 0, "subscribe filter 0 is nofilter")
+	flag.Parse()
 }
 
 func main() {
@@ -72,58 +72,63 @@ func main() {
 		}
 		go Send(&cli)
 		go Recv(&cli)
-        	time.Sleep(time.Duration(wakeIntval) * time.Millisecond)
+		time.Sleep(time.Duration(wakeIntval) * time.Millisecond)
 	}
 	for {
 		//reader := bufio.NewReader(os.Stdin)
 		//fmt.Print(">")
 		//text, _ := reader.ReadString('\n')
 		//Command(text)
-        	time.Sleep(0 * time.Second)
+		time.Sleep(0 * time.Second)
 	}
 }
 
-func Command(text string){
+func Command(text string) {
 	cmds := strings.Split(text, " ")
-	if len(cmds) < 2  {
+	if len(cmds) < 2 {
 		fmt.Println("command not found")
 	}
 	switch cmds[0] {
-		case "msgsize": SetMsgSize(cmds[1])
-		case "intval": SetIntval(cmds[1])
-		default: fmt.Println("command not found")
+	case "msgsize":
+		SetMsgSize(cmds[1])
+	case "intval":
+		SetIntval(cmds[1])
+	default:
+		fmt.Println("command not found")
 	}
 }
 
-func SetMsgSize(msg string){
-	size,_ := strconv.Atoi(msg)
+func SetMsgSize(msg string) {
+	size, _ := strconv.Atoi(msg)
 	if size < 2800 {
 		msgSize = size
-		fmt.Println("msg size reloaded "+msg)
+		fmt.Println("msg size reloaded " + msg)
 	} else {
 		fmt.Println("msg size over, size max is 2800")
 	}
 }
 
-func SetIntval(msg string){
-	interval,_ = strconv.Atoi(msg)
-	fmt.Println("interval reloaded "+msg)
+func SetIntval(msg string) {
+	interval, _ = strconv.Atoi(msg)
+	fmt.Println("interval reloaded " + msg)
 }
 
-func Send(cli *client){
+func Send(cli *client) {
 	var err error
-	cli.Deal, err = goczmq.NewDealer(destDealSchema+destAddr+destDealPort)
+	cli.Deal, err = goczmq.NewDealer(destDealSchema + destAddr + destDealPort)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer cli.Deal.Destroy()
 	for {
-		msg := strconv.Itoa(cli.Id)+","+strconv.Itoa(cli.Seq) + ","+cli.Msg[0:msgSize]
+		msg := strconv.Itoa(cli.Id) + "," + strconv.Itoa(cli.Seq) + "," + cli.Msg[0:msgSize]
 		err = cli.Deal.SendFrame([]byte(msg), goczmq.FlagNone)
 		if err != nil {
-		        log.Fatal(err)
+			log.Fatal(err)
 		}
-		if logLevel > 0 { log.Printf("<- %s", msg) }
+		if logLevel > 0 {
+			log.Printf("<- %s", msg)
+		}
 		// else { }
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 		cli.Seq += 1
@@ -131,9 +136,9 @@ func Send(cli *client){
 	cli.Deal.Destroy()
 }
 
-func Recv(cli *client){
+func Recv(cli *client) {
 	var err error
-	cli.Sub, err = goczmq.NewSub(destSubSchema+destAddr+destSubPort,cli.Filter)
+	cli.Sub, err = goczmq.NewSub(destSubSchema+destAddr+destSubPort, cli.Filter)
 	cli.Sub.SetSubscribe(cli.Filter)
 	if err != nil {
 		log.Fatal(err)
@@ -142,7 +147,7 @@ func Recv(cli *client){
 	for {
 		reply, err := cli.Sub.RecvMessage()
 		if err != nil {
-		        log.Fatal(err)
+			log.Fatal(err)
 		}
 		if logLevel > 0 {
 			log.Printf("-> %s", string(reply[0]))
@@ -163,7 +168,9 @@ func Recv(cli *client){
 							exists = true
 						}
 					}
-					if exists { list = append(list, cli.Seq) }
+					if exists {
+						list = append(list, cli.Seq)
+					}
 					cli.Seqgap = list
 					//log.Printf("> self seq lost id:%s seq:%s", res[1], res[2])
 					log.Printf("> delay count :%d", len(list))
--- src/relay.go
+++ src/relay.go
@@ -1,58 +1,57 @@
 package main
 
 import (
-	"log"
-	"time"
 	"flag"
 	"github.com/zeromq/goczmq"
-	"strings"
-	"strconv"
+	"log"
+	"time"
 )
 
 var (
-    destDealSchema string
-    destDealPort string
-    destSubSchema string
-    destSubPort string
-    logLevel int
+	destDealSchema string
+	destDealPort   string
+	destSubSchema  string
+	destSubPort    string
+	logLevel       int
 )
+
 func param() {
-    flag.StringVar(&destDealSchema, "dschm","tcp://", "destination dealer schema tcp or udp")
-    flag.StringVar(&destDealPort, "dport",":7001", "destination dealer port")
-    flag.StringVar(&destSubSchema, "sschm","tcp://", "destination subscribe schema tcp or udp")
-    flag.StringVar(&destSubPort, "sport",":7000", "destination subscribe port")
-    flag.IntVar(&logLevel, "log",0 ,"loglevel ... 0=lostonly, 1=verbose")
-    flag.Parse()
+	flag.StringVar(&destDealSchema, "dschm", "tcp://", "destination dealer schema tcp or udp")
+	flag.StringVar(&destDealPort, "dport", ":7001", "destination dealer port")
+	flag.StringVar(&destSubSchema, "sschm", "tcp://", "destination subscribe schema tcp or udp")
+	flag.StringVar(&destSubPort, "sport", ":7000", "destination subscribe port")
+	flag.IntVar(&logLevel, "log", 0, "loglevel ... 0=lostonly, 1=verbose")
+	flag.Parse()
 }
 
 func main() {
 	param()
-	router, err := goczmq.NewRouter(destDealSchema+"*"+destDealPort)
+	router, err := goczmq.NewRouter(destDealSchema + "*" + destDealPort)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer router.Destroy()
 
-        pub, err := goczmq.NewPub(destSubSchema+"*"+destSubPort)
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer pub.Destroy()
-    for {
-	request, err := router.RecvMessage()
+	pub, err := goczmq.NewPub(destSubSchema + "*" + destSubPort)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//log.Printf("router received '%s' from '%v'", request[1], request[0])
-	if logLevel > 0 { 
-		log.Printf("-> %s", request[1])
-	}
-	
-	err = pub.SendFrame([]byte("0,"+string(request[1])), goczmq.FlagNone)
-        if err != nil {
-                log.Fatal(err)
-        }
+	defer pub.Destroy()
+	for {
+		request, err := router.RecvMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+		//log.Printf("router received '%s' from '%v'", request[1], request[0])
+		if logLevel > 0 {
+			log.Printf("-> %s", request[1])
+		}
 
-        time.Sleep(0 * time.Second) // return context
-    }
+		err = pub.SendFrame([]byte("0,"+string(request[1])), goczmq.FlagNone)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(0 * time.Second) // return context
+	}
 }
